89ee1d547e8828011dbbea4ca3169e863e913b22
weijiew <836678589@qq.com>
2024-02-11 00:35:33 +0800

    feat: Add unpacking function for instruction parsing.

--- src/instructions.cpp
+++ src/instructions.cpp
@@ -16,66 +16,67 @@ namespace std {
 
 namespace crvemu {
 
-std::optional<uint64_t> executeAddi(Cpu& cpu, uint32_t inst) {
-    uint32_t rd = (inst >> 7) & 0x1f;
-    uint32_t rs1 = (inst >> 15) & 0x1f;
-    int64_t immediate = static_cast<int32_t>(inst & 0xfff00000) >> 20;
-
-    std::cout << "ADDI: x" << rd << " = x" << rs1 << " + " << immediate << std::endl;
-    cpu.regs[rd] = cpu.regs[rs1] + immediate;
-    return cpu.update_pc();
+// 拆包解包
+std::tuple<uint32_t, uint32_t, uint32_t> unpackInstruction(uint32_t inst) {
+  return {
+    (inst >> 7) & 0x1f,   // rd
+    (inst >> 15) & 0x1f,  // rs1
+    (inst >> 20) & 0x1f  // rs2
+  };
 }
 
-std::optional<uint64_t> executeSlli(Cpu& cpu, uint32_t inst) {
-    uint32_t rd = (inst >> 7) & 0x1f;
-    uint32_t rs1 = (inst >> 15) & 0x1f;
-    int64_t immediate = static_cast<int32_t>(inst & 0xfff00000) >> 20;
+std::optional<uint64_t> executeAddi(Cpu& cpu, uint32_t inst) {
+  auto [rd, rs1, rs2] = unpackInstruction(inst);
 
-    std::cout << "SLLI: x" << rd << " = x" << rs1 << " << " << (immediate & 0x3f) << std::endl;
-    cpu.regs[rd] = cpu.regs[rs1] << (immediate & 0x3f);
-    return cpu.update_pc();
+  auto immediate = static_cast<int64_t>(static_cast<int32_t>(inst & 0xfff00000) >> 20);
+  std::cout << "ADDI: x" << rd << " = x" << rs1 << " + " << immediate << std::endl;
+  cpu.regs[rd] = cpu.regs[rs1] + immediate;
+  return cpu.update_pc();
+}
+
+
+std::optional<uint64_t> executeSlli(Cpu& cpu, uint32_t inst) {
+  auto [rd, rs1, rs2] = unpackInstruction(inst);
+
+  auto immediate = static_cast<int64_t>(static_cast<int32_t>(inst & 0xfff00000) >> 20);
+  std::cout << "SLLI: x" << rd << " = x" << rs1 << " << " << (immediate & 0x3f) << std::endl;
+  cpu.regs[rd] = cpu.regs[rs1] << (immediate & 0x3f);
+  return cpu.update_pc();
 }
 
 std::optional<uint64_t> executeSlti(Cpu& cpu, uint32_t inst) {
-    uint32_t rd = (inst >> 7) & 0x1f;
-    uint32_t rs1 = (inst >> 15) & 0x1f;
+  auto [rd, rs1, rs2] = unpackInstruction(inst);
+  auto immediate = static_cast<int64_t>(static_cast<int32_t>(inst & 0xfff00000) >> 20);
 
-    int64_t imm = static_cast<int32_t>(inst & 0xfff00000) >> 20;
-    std::cout << "SLTI: x" << rd << " = (x" << rs1 << " < " << imm << ") ? 1 : 0" << std::endl;
-    cpu.regs[rd] = (cpu.regs[rs1] < static_cast<uint64_t>(imm)) ? 1 : 0;
-    return cpu.update_pc();
+  std::cout << "SLTI: x" << rd << " = (x" << rs1 << " < " << immediate << ") ? 1 : 0" << std::endl;
+  cpu.regs[rd] = (cpu.regs[rs1] < static_cast<uint64_t>(immediate)) ? 1 : 0;
+  return cpu.update_pc();
 }
 
-
 std::optional<uint64_t> executeSltiu(Cpu& cpu, uint32_t inst) {
-    uint32_t rd = (inst >> 7) & 0x1f;
-    uint32_t rs1 = (inst >> 15) & 0x1f;
+  auto [rd, rs1, rs2] = unpackInstruction(inst);
+  auto immediate = static_cast<int64_t>(static_cast<int32_t>(inst & 0xfff00000) >> 20);
 
-    int64_t imm = static_cast<int32_t>(inst & 0xfff00000) >> 20;
-    std::cout << "SLTIU: x" << rd << " = (x" << rs1 << " < " << imm << ") ? 1 : 0" << std::endl;
-    cpu.regs[rd] = (cpu.regs[rs1] < static_cast<unsigned int>(imm)) ? 1 : 0;
-    return cpu.update_pc();
+  std::cout << "SLTIU: x" << rd << " = (x" << rs1 << " < " << immediate << ") ? 1 : 0" << std::endl;
+  cpu.regs[rd] = (cpu.regs[rs1] < static_cast<unsigned int>(immediate)) ? 1 : 0;
+  return cpu.update_pc();
 }
 
 std::optional<uint64_t> executeXori(Cpu& cpu, uint32_t inst) {
-  uint32_t rd = (inst >> 7) & 0x1f;
-  uint32_t rs1 = (inst >> 15) & 0x1f;
-  uint32_t rs2 = (inst >> 20) & 0x1f;
-  int64_t imm = static_cast<int32_t>(inst & 0xfff00000) >> 20;
-  std::cout << "XORI: x" << rd << " = x" << rs1 << " ^ " << imm << std::endl;
-  cpu.regs[rd] = cpu.regs[rs1] ^ imm;
+  auto [rd, rs1, rs2] = unpackInstruction(inst);
+  auto immediate = static_cast<int64_t>(static_cast<int32_t>(inst & 0xfff00000) >> 20);
+
+  std::cout << "XORI: x" << rd << " = x" << rs1 << " ^ " << immediate << std::endl;
+  cpu.regs[rd] = cpu.regs[rs1] ^ immediate;
   return cpu.update_pc();
 }
 
 std::optional<uint64_t> executeSrli(Cpu& cpu, uint32_t inst) {
-  uint32_t rd = (inst >> 7) & 0x1f;
-  uint32_t rs1 = (inst >> 15) & 0x1f;
+  auto [rd, rs1, rs2] = unpackInstruction(inst);
+  auto immediate = static_cast<int64_t>(static_cast<int32_t>(inst & 0xfff00000) >> 20);
 
-  // imm[11:0] = inst[31:20]
-  uint64_t imm = static_cast<int32_t>((static_cast<int32_t>(inst & 0xfff00000) >> 20) & 0xfff);
-
-  // "The shift amount is encoded in the lower 6 bits of the I-immediate field for RV64I."
-  uint32_t shamt = static_cast<uint32_t>(imm & 0x3f);
+  // "对于 RV64I，移位量被编码在 I-immediate 字段的低 6 位中。"
+  uint32_t shamt = static_cast<uint32_t>(immediate & 0x3f);
 
   std::cout << "SRLI: x" << rd << " = x" << rs1 << " >> " << shamt << std::endl;
   cpu.regs[rd] = cpu.regs[rs1] >> shamt;
@@ -83,15 +84,12 @@ std::optional<uint64_t> executeSrli(Cpu& cpu, uint32_t inst) {
 }
 
 std::optional<uint64_t> executeSrai(Cpu& cpu, uint32_t inst) {
-  uint32_t rd = (inst >> 7) & 0x1f;
-  uint32_t rs1 = (inst >> 15) & 0x1f;
-
-  // imm[11:0] = inst[31:20]
-  uint64_t imm = static_cast<uint64_t>((static_cast<int32_t>(inst & 0xfff00000) >> 20) & 0xfff);
+  auto [rd, rs1, rs2] = unpackInstruction(inst);
+  auto immediate = static_cast<int64_t>(static_cast<int32_t>(inst & 0xfff00000) >> 20);
 
   // 在 I 类型指令中，immediate 字段（I-immediate field）通常用来表示一个立即数，
   // 而对于右移类指令，如算术右移指令（srai），这个立即数的低6位通常用来表示右移的位数。
-  uint32_t shamt = static_cast<uint32_t>(imm & 0x3f);
+  uint32_t shamt = static_cast<uint32_t>(immediate & 0x3f);
 
   std::cout << "SRAI: x" << rd << " = x" << rs1 << " >> " << shamt << " (arithmetic right shift)" << std::endl;
   cpu.regs[rd] = static_cast<uint64_t>(static_cast<int64_t>(cpu.regs[rs1]) >> shamt);
@@ -116,31 +114,26 @@ std::optional<uint64_t> executefunct70X5(Cpu& cpu, uint32_t inst) {
 }
 
 std::optional<uint64_t> executeOri(Cpu& cpu, uint32_t inst) {
-  uint32_t rd = (inst >> 7) & 0x1f;
-  uint32_t rs1 = (inst >> 15) & 0x1f;
+  auto [rd, rs1, rs2] = unpackInstruction(inst);
+  auto immediate = static_cast<int64_t>(static_cast<int32_t>(inst & 0xfff00000) >> 20);
 
-  // imm[11:0] = inst[31:20]
-  uint64_t imm = static_cast<uint64_t>((static_cast<int32_t>(inst & 0xfff00000) >> 20) & 0xfff);
-  std::cout << "ORI: x" << rd << " = x" << rs1 << " | " << imm << std::endl;
-  cpu.regs[rd] = cpu.regs[rs1] | imm;
+  std::cout << "ORI: x" << rd << " = x" << rs1 << " | " << immediate << std::endl;
+  cpu.regs[rd] = cpu.regs[rs1] | immediate;
   return cpu.update_pc();
 }
 
 std::optional<uint64_t> executeAndi(Cpu& cpu, uint32_t inst) {
-  uint32_t rd = (inst >> 7) & 0x1f;
-  uint32_t rs1 = (inst >> 15) & 0x1f;
+  auto [rd, rs1, rs2] = unpackInstruction(inst);
+  auto immediate = static_cast<int64_t>(static_cast<int32_t>(inst & 0xfff00000) >> 20);
 
-  // imm[11:0] = inst[31:20]
-  uint64_t imm = static_cast<uint64_t>((static_cast<int32_t>(inst & 0xfff00000) >> 20) & 0xfff);
-  std::cout << "ANDI: x" << rd << " = x" << rs1 << " & " << imm << std::endl;
-  cpu.regs[rd] = cpu.regs[rs1] & imm;
+  std::cout << "ANDI: x" << rd << " = x" << rs1 << " & " << immediate << std::endl;
+  cpu.regs[rd] = cpu.regs[rs1] & immediate;
   return cpu.update_pc();
 }
 
 std::optional<uint64_t> executeAdd(Cpu& cpu, uint32_t inst) {
-  uint32_t rd = (inst >> 7) & 0x1f;
-  uint32_t rs1 = (inst >> 15) & 0x1f;
-  uint32_t rs2 = (inst >> 20) & 0x1f;
+  auto [rd, rs1, rs2] = unpackInstruction(inst);
+
   std::cout << "ADD: x" << rd << " = x" << rs1 << " + x" << rs2 << std::endl;
   cpu.regs[rd] = cpu.regs[rs1] + cpu.regs[rs2];
   return cpu.update_pc();
